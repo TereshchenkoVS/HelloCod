c9450efd66f33c5266e0b326ceb35c762a8f3f28
TereshchenkoVS <110491877+TereshchenkoVS@users.noreply.github.com>
2022-08-06 12:54:35 +0300

    add ifElse

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string  username = Console.ReadLine();
+
+if(username.ToLower() == "лилия")
+{
+    Console.WriteLine("Ура, это же ЛИЛИЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
